9d90ebc18e388c8b410428ee4230d9664ed82f85
Mohammad-Gouse <95171652+Mohammad-Gouse@users.noreply.github.com>
2022-09-09 00:07:02 +0530

    Student program modified.

--- Student.java
+++ Student.java
@@ -6,12 +6,16 @@ class Student
 	{
 		int id;
 		String sname;
+		char attendence;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter student id: ");
 		id = sc.nextInt();
 		System.out.println("Enter student name: ");
 		sname = sc.next();
+		System.out.println("Enter Attendence P or A: ");
+		attendence = sc.next().charAt(0);
 		System.out.println("id: "+id);
 		System.out.println("name: "+sname);
+		System.out.println("Attendence: "+attendence);
 	}
 }
